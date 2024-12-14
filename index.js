6b9d3cdaade17d786d0c025a4f7ab957d6a038fa
Javier Granja San Martín <ja.granja@duocuc.cl>
2024-12-14 10:54:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,8 @@
-const jsonServer = require('json-server');
+const jsonServer=require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('almacen.json'); // Archivo JSON
+const router = jsonServer.router("almacen.json");
 const middlewares = jsonServer.defaults();
-
-// Middleware
+const port = process.env.PORT || 10000;
 server.use(middlewares);
-server.use(jsonServer.bodyParser);
-
-// Personalizar POST para el endpoint "/users"
-server.post('/users', (req, res) => {
-  const users = router.db.get('users');
-  const newUser = req.body;
-
-  // Validar campos únicos, como email
-  if (users.find({ email: newUser.email }).value()) {
-    return res.status(400).send({ message: 'El email ya está registrado.' });
-  }
-
-  users.push(newUser).write();
-  res.status(201).send(newUser);
-});
-
 server.use(router);
-server.listen(3000, () => {
-  console.log('JSON Server está corriendo en el puerto 3000');
-});
+server.listen(port);
