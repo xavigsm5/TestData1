acfccd195d21a309883dd2be80510bd396c2c823
Javier Granja San Martín <ja.granja@duocuc.cl>
2024-12-15 22:27:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,32 +5,32 @@ const router = jsonServer.router("almacen.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 10000;
 
-// Configuración del middleware JSON Server
+
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 
-// Configuración de transporte para enviar correos (Gmail)
+
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "tu_correo@gmail.com", // Tu correo Gmail
-    pass: "tu_contraseña_o_app_password", // Contraseña o App Password
+    user: "tvmagis1515", 
+    pass: "magistv1515", 
   },
 });
 
-// Endpoint personalizado para enviar correos
-server.post("/send-recovery-email", (req, res) => {
-  const email = req.body.email; // Email que se envía desde el cliente
 
-  // Leer el archivo almacen.json
-  const users = router.db.get("users").value(); // Nombre del array en almacen.json
+server.post("/send-recovery-email", (req, res) => {
+  const email = req.body.email; 
+
+  
+  const users = router.db.get("users").value(); 
   const user = users.find((u) => u.email === email);
 
   if (user) {
-    // Token de ejemplo (deberías generarlo dinámicamente)
+    
     const token = "de3bwgnm15g";
 
-    // Configuración del correo
+   
     const mailOptions = {
       from: "tu_correo@gmail.com",
       to: email,
@@ -38,7 +38,7 @@ server.post("/send-recovery-email", (req, res) => {
       text: `Tu token de recuperación es: ${token}`,
     };
 
-    // Enviar correo
+    
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
         console.log(error);
@@ -54,10 +54,10 @@ server.post("/send-recovery-email", (req, res) => {
   }
 });
 
-// Usar json-server router
+
 server.use(router);
 
-// Iniciar servidor
+
 server.listen(port, () => {
   console.log(`JSON Server está corriendo en el puerto ${port}`);
 });
